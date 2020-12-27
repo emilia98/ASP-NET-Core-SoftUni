74aa8706e8f2660ee4af66cda2e72587a066b9cf
emilia98 <emilia_nedialkova@abv.bg>
2020-12-27 23:57:12 +0200

    add PropertyController

--- HomeForMe/HomeForMe/Web/HomeForMe.API/Controllers/PropertyController.cs
+++ HomeForMe/HomeForMe/Web/HomeForMe.API/Controllers/PropertyController.cs
@@ -1,16 +1,223 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HomeForMe.Data;
+using HomeForMe.Data.Models;
+using HomeForMe.InputModels.Property;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HomeForMe.API.Controllers
 {
-    public class PropertyController : Controller
+    public class PropertyController : BaseAPIController
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _dbContext;
+
+        public PropertyController(ApplicationDbContext dbContext)
         {
-            return View();
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var properties = await _dbContext.Properties.ToListAsync();
+
+            return Ok(new
+            {
+                Properties = properties
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var property = await _dbContext.Properties.FindAsync(id);
+
+            if (property == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Property does not exist!",
+                    HasError = true
+                });
+            }
+
+            return Ok(new
+            {
+                Property = property
+            });
+        }
+
+
+        [Authorize]
+        [HttpGet("my")]
+        public async Task<IActionResult> GetAllByUser()
+        {
+            var username = this.User.FindFirst(ClaimTypes.Name)?.Value;
+
+            var userByUsername = await _dbContext.Users.FirstOrDefaultAsync(u => u.UserName == username);
+
+            if (userByUsername == null)
+            {
+                return Unauthorized(new
+                {
+                    Message = "Something went wrong while fetching your properties!",
+                    HasError = true
+                });
+            }
+
+            var properties = await _dbContext.Properties.Where(x => x.UserId == userByUsername.Id).ToListAsync();
+
+            return Ok(new
+            {
+                Properties = properties
+            });
+        }
+
+        [HttpGet("new/data")]
+        public async Task<IActionResult> GetNewPropertyInfo()
+        {
+            var locations = await _dbContext.Locations.ToListAsync();
+            var propertyTypes = await _dbContext.PropertyTypes.ToListAsync();
+
+            return Ok(new
+            {
+                Locations = locations,
+                PropertyTypes = propertyTypes
+            });
+        }
+
+        [Authorize]
+        [HttpPost("new")]
+        public async Task<IActionResult> New(NewPropertyInputModel propertyInputModel)
+        {
+            var locationId = propertyInputModel.Location;
+            var location = await _dbContext.Locations.FirstOrDefaultAsync(l => l.Id == locationId);
+
+            if (location == null)
+            {
+                return BadRequest(new
+                {
+                    Message = "Invalid location!",
+                    HasFormError = true
+                });
+            }
+
+            var typeId = propertyInputModel.Type;
+            var propertyType = await _dbContext.PropertyTypes.FirstOrDefaultAsync(t => t.Id == typeId);
+
+            if (propertyType == null)
+            {
+                return BadRequest(new
+                {
+                    Message = "Invalid property type!",
+                    HasFormError = true
+                });
+            }
+
+            var user = await this.GetUser();
+
+            if (user == null)
+            {
+                return Unauthorized(new
+                {
+                    Message = "Something went wrong while fetching your properties!",
+                    HasError = true
+                });
+            }
+
+            var property = new Property
+            {
+                LocationId = locationId.Value,
+                PropertyTypeId = typeId.Value,
+                UserId = user.Id,
+                Price = propertyInputModel.Price.Value,
+                Bedrooms = propertyInputModel.Bedrooms.Value,
+                Bathrooms = propertyInputModel.Bathrooms.Value,
+                Description = propertyInputModel.Description,
+                AddedAt = DateTime.Now,
+                ForRent = true
+            };
+
+            try
+            {
+                await _dbContext.Properties.AddAsync(property);
+                await _dbContext.SaveChangesAsync();
+            } 
+            catch
+            {
+                return BadRequest(new
+                {
+                    Message = "An error occurred while trying to add new property!",
+                    HasError = true
+                });
+            }
+
+            return Ok(new
+            {
+                SuccessMessage = "Successfully added a new property!",
+                HasSuccess = true
+            });
+        }
+
+        [Authorize]
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var property = await _dbContext.Properties.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (property == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Property does not exist!",
+                    HasError = true
+                });
+            }
+
+            var user = await this.GetUser();
+
+            if (user == null || user.Id != property.UserId)
+            {
+                return Unauthorized(new
+                {
+                    Message = "Cannot process deleting a property!",
+                    HasError = true
+                });
+            }
+
+            try
+            {
+                _dbContext.Properties.Remove(property);
+                await _dbContext.SaveChangesAsync();
+            } 
+            catch
+            {
+                return BadRequest(new
+                {
+                    Message = "Something went wrong while deleting a property!",
+                    HasError = true
+                });
+            }
+
+            return Ok(new
+            {
+                Message = "Successfully deleted a property!",
+                HasSuccess = true
+            });
+        }
+
+        
+
+        private async Task<AppUser> GetUser()
+        {
+            var username = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            return await this._dbContext.Users.FirstOrDefaultAsync(u => u.UserName == username);
         }
     }
 }
---
+++ HomeForMe/HomeForMe/Web/HomeForMe.InputModels/Property/NewPropertyInputModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeForMe.InputModels.Property
+{
+    public class NewPropertyInputModel
+    {
+        [Required]
+        public int? Location { get; set; }
+
+        [Required]
+        public int? Type { get; set; }
+
+        [Required]
+        [Range(0, 100000, ErrorMessage = "Price should be a value between 0 and 100000!")]
+        public decimal? Price { get; set; }
+
+        [Required]
+        [Range(0, 20, ErrorMessage = "Bedrooms should be a value between 0 and 20!")]
+        public int? Bedrooms { get; set; }
+
+        [Required]
+        [Range(0, 20, ErrorMessage = "Bathrooms should be a value between 0 and 20!")]
+        public int? Bathrooms { get; set; }
+
+        public string Description { get; set; }
+    }
+}
