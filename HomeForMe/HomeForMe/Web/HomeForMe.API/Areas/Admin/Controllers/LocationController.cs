4860303612e7a1ec8c0d9af5d0e41795bd8e8286
emilia98 <emilia_nedialkova@abv.bg>
2020-12-29 07:57:11 +0200

    add LocationController

--- HomeForMe/HomeForMe/Web/HomeForMe.API/Areas/Admin/Controllers/LocationController.cs
+++ HomeForMe/HomeForMe/Web/HomeForMe.API/Areas/Admin/Controllers/LocationController.cs
@@ -1,4 +1,8 @@
-﻿using HomeForMe.Services.Data.Contracts;
+﻿using HomeForMe.Data.Models;
+using HomeForMe.InputModels.Admin;
+using HomeForMe.Services.Data.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace HomeForMe.API.Areas.Admin.Controllers
 {
@@ -10,5 +14,111 @@ namespace HomeForMe.API.Areas.Admin.Controllers
         {
             _locationService = locationService;
         }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAll()
+        {
+            var locations = await _locationService.GetAll<Location>(true);
+
+            return Ok(new
+            {
+                Locations = locations
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var location = await _locationService.GetById<Location>(id, true);
+
+            if (location == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Location does not exist!",
+                    HasError = true
+                });
+            }
+
+            return Ok(new
+            {
+                Location = location
+            });
+        }
+
+        [HttpPost("new")]
+        public async Task<IActionResult> New(LocationInputModel locationInputModel)
+        {
+            var location = new Location
+            {
+                City = locationInputModel.City
+            };
+
+            try
+            {
+                await _locationService.Create(location);
+            }
+            catch
+            {
+                return BadRequest(new
+                {
+                    Message = "An error occurred while adding a new location!",
+                    HasError = true
+                });
+            
+            }
+
+            return Ok(new
+            {
+                Message = "Successfully added a new location!",
+                HasSuccess = true
+            });
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var location = await _locationService.GetById<Location>(id, true);
+
+            if (location == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Location does not exist!",
+                    HasError = true
+                });
+            }
+
+            var action = "deleting";
+
+            try
+            {
+                if (location.IsDeleted)
+                {
+                    await _locationService.Undelete(location);
+                    action = "removing from deleted!";
+                }
+                else
+                {
+                    await _locationService.Delete(location);
+                }
+            }
+            catch
+            {
+                return BadRequest(new
+                {
+                    Message = $"An error occurred while {action} a location!",
+                    HasError = true
+                });
+            }
+
+            return Ok(new
+            {
+                Message = $"Successfully {action} a location!",
+                HasSuccess = true
+            });
+        }
+
+        //TODO: Add update action
     }
 }
---
+++ HomeForMe/HomeForMe/Web/HomeForMe.InputModels/Admin/LocationInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeForMe.InputModels.Admin
+{
+    public class LocationInputModel
+    {
+        [Required]
+        [StringLength(30, ErrorMessage = "City should be at most 30 characters long!")]
+        public string City { get; set; }
+    }
+}
